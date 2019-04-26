1376cbeeec2f44ae443391cc29c6ca1bbf6ca454
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-04-25 19:20:31 -0700

    styled workcard modal

--- client/src/components/WorkCards/WorkModal/WorkModal.js
+++ client/src/components/WorkCards/WorkModal/WorkModal.js
@@ -12,16 +12,18 @@ export default function WorkModal(props) {
 
     <div className={`WorkModal__container ${show}`} style={{...props.style}}>
       <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={props.data.shown}>
-          
-          <div className="WorkModal__header-img" style={{backgroundImage: `url(${props.data.img})`}}></div>
               <div className="WorkModal__inner-wrapper">
-                  <h2>{props.data.title}</h2>
-                  <p>{props.data.about}</p>
-                  <h3>Description:</h3>
-                    <p>{props.data.description}</p>
-                  <h3>Technologies:</h3>
-                    <p>{props.data.tech.join(', ')}</p>
-                  <a href={props.data.url}>Visit</a>
+                  <div className="WorkModal__header-img" style={{backgroundImage: `url(${props.data.img})`}}>
+                    {/* <h2>{props.data.title}</h2> */}
+                  </div>
+                  <div style={{padding: '20px'}}>
+                    <p>{props.data.title}</p>
+                    <h3>Description:</h3>
+                      <p>{props.data.description}</p>
+                    <h3>Technologies:</h3>
+                      <p>{props.data.tech.join(', ')}</p>
+                    <a href={props.data.url}>Visit</a>
+                  </div>
               </div>
               <div className="WorkModal-close" onClick={props.click}><img src={closeButton} alt="Close Modal"></img></div>
               </Animated>        
